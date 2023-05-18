e8867a95ff5a88c980ab5ec9d2634d054d83ff3e
giantyo26 <72968195+giantyo26@users.noreply.github.com>
2023-05-18 19:37:57 +0700

    Separate maxRedigit function, so it can be immediately callable with the input value

--- maxRedigit/main.go
+++ maxRedigit/main.go
@@ -5,30 +5,29 @@ import (
 )
 
 func main() {
-	var num int
+	maxRedigit(1000)
+	maxRedigit(0)
+	maxRedigit(123)
+	maxRedigit(879)
+}
 
-	fmt.Print("Input 3 digit number: ")
-	// Get input from user
-	_, err := fmt.Scan(&num)
-	if err != nil {
-		fmt.Println("Invalid input")
-	}
+func maxRedigit(num int) {
 	// Check if its 3 digit positive int, if true, rearrange the digit from the biggest num
-	if num >= 100 || num <= 999 {
+	if num >= 100 && num <= 999 {
 		nums := make([]int, 0, 3)
 		for num > 0 {
 			nums = append(nums, num%10) // Get each digit with mod & push the remainder into the slice e.g = 792 % 10 = 2
-			num /= 10 // e.g = 792 / 10 = 79, 79 / 10 = 7
+			num /= 10                   // e.g = 792 / 10 = 79, 79 / 10 = 7
 		}
 
 		sortedNums := sortNums(nums)
-		fmt.Printf("result: %d\n", sortedNums)
+		fmt.Printf("%d\n", sortedNums)
 	} else {
 		fmt.Println(nil)
 	}
 }
 
-// Sorting by comparing each digit and swapping them
+// Sorting by comparing 2 int and swapping them
 func sortNums(nums []int) int {
 	var N = len(nums)
 	var x = 0
